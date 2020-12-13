bd75a6c57292dd7ceaeae603d760ea3309e9f5bf
tylerzey <tylerzey@gmail.com>
2020-12-12 19:03:32 -0600

    log out connectino event

--- src/entities/connections/model.ts
+++ src/entities/connections/model.ts
@@ -2,13 +2,23 @@ import { twoHoursInSeconds } from "../../common/dateFunctions";
 import { storeItem } from "../../common/dynamo";
 import { dynamoLabels, dynamoSeparator } from "../../common/dynamoHelpers";
 
-export async function createConnection(connectionId: string) {
+export async function createConnection(
+  connectionId: string,
+  {
+    connectedAt,
+    domain,
+    stage,
+  }: { connectedAt?: number; domain?: string; stage?: string }
+) {
   console.log("createConnection");
 
   const connection = {
     key: `${dynamoLabels.connection}${dynamoSeparator}${connectionId}`,
     secondaryKey: `${dynamoLabels.connection}${dynamoSeparator}${connectionId}`,
     expiresAfter: Date.now() + (twoHoursInSeconds || 0),
+    connectedAt,
+    domain,
+    stage,
   };
 
   await storeItem(connection);
--- src/handlers/connectionHandler.ts
+++ src/handlers/connectionHandler.ts
@@ -1,17 +1,21 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { createConnection } from "../entities/connections/model";
 
 export async function handler(
   event: APIGatewayEvent
 ): Promise<APIGatewayProxyResult> {
+  console.log("connections handler");
+  console.log(event);
+
   if (!event.requestContext?.connectionId) {
     return { statusCode: 500, body: "Missing connectionId" };
   }
-
   const connectionId = event?.requestContext?.connectionId;
   const connectedAt = event?.requestContext?.connectedAt;
   const domain = event?.requestContext?.domainName;
   const stage = event?.requestContext?.stage;
-  // store connection in ddb
+
+  await createConnection(connectionId, { connectedAt, domain, stage });
 
   return { statusCode: 200, body: "Connected." };
 }
